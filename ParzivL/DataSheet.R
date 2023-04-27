8f8f63b34aee65542892d494194620ccb3644b24
ParZivaL782 <56859883+ParZivaL782@users.noreply.github.com>
2023-04-27 10:38:25 +0530

    Add files via upload

---
+++ ParzivL/DataSheet.R
@@ -0,0 +1,4 @@
+data<-read.csv("C:\\Users\\autblt411\\Downloads\\iris_csv.csv")
+print(data,header=T)
+head(data)
+
---
+++ ParzivL/if_else.R
@@ -0,0 +1,25 @@
+a<-23
+b<-4
+marks<-50
+if(marks>=50){
+  if(marks>=90)
+  {
+    print("Passed A+")
+  }else if(marks>=80)
+  {
+    print("Passed A")
+  }else if(marks>70)
+  {
+    print("Passed B+")
+  }else if(marks>60)
+  {
+    print("Passed B")
+  }else
+  {
+    print("Just Passed")
+  }
+}else
+{
+  print("Fail")
+}
+
